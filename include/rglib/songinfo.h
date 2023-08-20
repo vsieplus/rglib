c3aa9947ab9315c36b3cf9c454d36dc1c7e19ea3
vsieplus <35880073+vsieplus@users.noreply.github.com>
2023-08-20 10:56:32 -0700

    SongInfo | Add default JSON parsing

--- include/rglib/songinfo.h
+++ include/rglib/songinfo.h
@@ -13,17 +13,21 @@ public:
     enum class FileFormat{
         INI,
         JSON,
-        UNKNOWN,
+        CUSTOM,
     };
 
     SongInfo(fs::path filepath, FileFormat songinfoFormat);
-    SongInfo(float previewStart, float previewStop, std::string_view title, std::string_view artist,
+    SongInfo(double previewStart, double previewStop, std::string_view title, std::string_view artist,
         std::string_view genre, fs::path musicFilepath, fs::path artFilepath);
 
     virtual void save(fs::path saveDir, std::string_view filename, FileFormat songinfoFormat);
+protected:
+    virtual void loadFromINI(fs::path filepath);
+    virtual void loadFromJSON(fs::path filepath);
+    virtual void loadFromCustom(fs::path filepath);
 private:
-    float previewStart{};
-    float previewStop{};
+    double previewStart{};
+    double previewStop{};
 
     std::string title{};
     std::string artist{};
